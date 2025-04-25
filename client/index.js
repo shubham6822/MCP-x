20af18902bbf8bd3c852e92917797290e5d5ce9d
Shubham6822 <sharmashubham6822@gmail.com>
2025-04-25 10:47:27 +0530

    Adds function calling capability

--- client/index.js
+++ client/index.js
@@ -7,7 +7,7 @@ import { SSEClientTransport } from "@modelcontextprotocol/sdk/client/sse.js"
 
 config()
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
-
+let tools = []
 
 const chatHistory = [];
 const rl = readline.createInterface({
@@ -15,6 +15,7 @@ const rl = readline.createInterface({
     output: process.stdout,
 });
 
+// Function to handle function calls
 let client;
 const baseUrl = new URL("http://localhost:3001/sse");
 try {
@@ -24,32 +25,90 @@ try {
     });
     const sseTransport = new SSEClientTransport(baseUrl);
     await client.connect(sseTransport);
-    let tools = (await client.listTools()).tools
-    console.log("Connected using SSE transport", tools);
+    tools = (await client.listTools()).tools.map(tool => {
+        return {
+            name: tool.name,
+            description: tool.description,
+            parameters: {
+                type: tool.inputSchema.type,
+                properties: tool.inputSchema.properties,
+                required: tool.inputSchema.required
+            }
+        }
+    })
+
+    chatLoop()
+
 
 } catch (error) {
     console.error("Error connecting to server:", error);
     process.exit(1);
 }
 
-async function chatLoop() {
-    const question = await rl.question('You: ');
-    chatHistory.push({
-        role: "user",
-        parts: [
-            {
-                text: question,
-                type: "text"
-            }
-        ]
-    })
+// Function to handle function calls
+async function chatLoop(toolCall) {
+
+    // Get the last message from the chat history
+    if (toolCall) {
+
+        console.log("calling tool ", toolCall.name)
+
+        chatHistory.push({
+            role: "model",
+            parts: [
+                {
+                    text: `calling tool ${toolCall.name}`,
+                    type: "text"
+                }
+            ]
+        })
+
+        const toolResult = await client.callTool({
+            name: toolCall.name,
+            arguments: toolCall.args
+        })
+
+        chatHistory.push({
+            role: "user",
+            parts: [
+                {
+                    text: "Tool result : " + toolResult.content[0].text,
+                    type: "text"
+                }
+            ]
+        })
+
+    } else {
+        const question = await rl.question('You: ');
+        chatHistory.push({
+            role: "user",
+            parts: [
+                {
+                    text: question,
+                    type: "text"
+                }
+            ]
+        })
+    }
 
 
     const response = await ai.models.generateContent({
         model: "gemini-2.0-flash",
         contents: chatHistory,
+        config: {
+            tools: [
+                {
+                    functionDeclarations: tools,
+                }
+            ]
+        }
     })
     const responseText = response.candidates[0].content.parts[0].text
+    const functionCall = response.candidates[0].content.parts[0].functionCall
+
+    if (functionCall) {
+        return chatLoop(functionCall)
+    }
 
 
     chatHistory.push({
@@ -65,13 +124,10 @@ async function chatLoop() {
     console.log(`AI: ${responseText}`)
     chatLoop()
 
-    if (question === "exit") {
-        rl.close();
-        return;
-    }
+    // if (question === "exit") {
+    //     rl.close();
+    //     return;
+    // }
 
 }
-
-
-chatLoop()
 
